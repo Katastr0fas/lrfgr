d71e91f6a514477b3fd43cb2b69e604f7860a469
Katastrofas <117344267+Katastr0fas@users.noreply.github.com>
2023-03-27 13:40:13 +0300

    Добавьте файлы проекта.

---
+++ CoffeeHouse/DB/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeHouse.DB
+{
+    public static class AppData
+    {
+        public static CoffeeHouseEntities1 db = new CoffeeHouseEntities1();
+    }
+}
